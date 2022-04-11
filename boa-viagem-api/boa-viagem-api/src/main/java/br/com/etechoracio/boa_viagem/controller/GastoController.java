b6f164dd869ee532329e860a1f5c52467e1546fe
Luiz <bnaousaisso@gmail.com>
2022-04-11 08:48:33 -0300

    fazendo a requsicao do delete

--- boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
+++ boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
@@ -1,6 +1,7 @@
 package br.com.etechoracio.boa_viagem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +23,16 @@ public class GastoController {
 		return repository.findAll();
 
 	}
-	
+	//fazendo metodo de requisição da tabela gasto por id
 	@GetMapping("/{id}")
 	public Gasto buscarPorID(@PathVariable Long id) 
 	{
 		return repository.findById(id).orElse(null);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletarPorID(@PathVariable Long id) 
+	{
+		repository.findById(id);
 	}
 }
--- boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
+++ boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
@@ -26,26 +26,24 @@ public class Viagem {
 	@Column(name = "ID_VIAGEM")
 	private Long id;
 	
-//	@Column(name="TX_Destino")
-//	private String destino;
-//	
-//	@Enumerated(EnumType.STRING)
-//	@Column(name="TP_VIAGEM")
-//	private TipoViagemEnum tipo;
-//	
-//	@Column(name="DT_CHEGADA")
-//	private LocalDate chegada;
-//	
-//	@Column(name="DT_SAIDA")
-//	private LocalDate saida;
-//	
-//	@Column(name="VLR_ORCAMENTO")
-//	private Double orcamento;
-//	
-//	@Column(name="QTD_PESSOAS")
-//	private Integer pessoas;
+	@Column(name="TX_DESTINO")
+	private String destino;
 	
-	//teste
+	@Enumerated(EnumType.STRING)
+	@Column(name="TP_VIAGEM")
+	private TipoViagemEnum tipo;
+	
+	@Column(name="DT_CHEGADA")
+	private LocalDate chegada;
+	
+	@Column(name="DT_SAIDA")
+	private LocalDate saida;
+	
+	@Column(name="VLR_ORCAMENTO")
+	private Double orcamento;
+	
+	@Column(name="QTD_PESSOAS")
+	private Integer pessoas;
 	
 	
 }
